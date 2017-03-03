df15d8b0e92ac26af521570d41c1dda7a6b171c2
laverntius <narugo901@gmail.com>
2017-03-03 17:37:25 +0000

    yolo

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup(){
+    createCanvas(600,400);
+    background(200,200,200);
+}
+
+function draw(){
+    strokeWeight(8);
+    stroke(0,0,0);
+     fill(0,0,0);
+     line(100,100,100,400);
+     line (3,300,300,300);
+     line(3,399,200,200);
+     line(3,200,200,399);
+      fill(0,0,200);
+   stroke(0,0,200)
+    ellipse(300,300,200,50);
+      fill(200,0,0);
+    stroke(200,0,0);
+    ellipse(100,100,100,100);
+    fill(0,200,0);
+    stroke(0,200,0);
+     rect(200,100,100,100);
+}
